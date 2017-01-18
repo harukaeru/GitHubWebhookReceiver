10e05d63d1aee11bc95adaf2afe72916dd0c3402
Nakada Takumi <usrNeko@kerorin.local>
2017-01-18 12:06:11 +0900

    80

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn kerorin_module:kerorin_app
+web: sudo DEBUG=0 gunicorn -b 0.0.0.0:80 kerorin_module:kerorin_app
--- kerorin_module.py
+++ kerorin_module.py
@@ -1,8 +1,8 @@
-def kerorin_app(_, __):
-    data = '<h1>Hello, World! :)</h1>'
+def kerorin_app(_, start_response):
+    data = b'<h1>Hello, World! :)</h1>'
     status = '200 OK'
     response_headers = [
-        ('Content-type','text/plain'),
+        ('Content-type', 'text/html'),
         ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
